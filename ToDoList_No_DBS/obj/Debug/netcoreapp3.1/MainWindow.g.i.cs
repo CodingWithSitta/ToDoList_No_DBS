70d8cd0784648341c137d8919773f84f9995c88e
jbmaene1998 <jean-baptiste.maene@student.howest.be>
2021-01-25 12:16:12 +0100

    Test Github Connection

--- ToDoList_No_DBS/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ ToDoList_No_DBS/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace ToDoList_No_DBS {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ToDoList_No_DBS;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ToDoList_No_DBS;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
