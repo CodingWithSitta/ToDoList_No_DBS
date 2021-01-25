457809f4579e23dfb6dbbf9626622b2cbe9b226b
jbmaene1998 <jean-baptiste.maene@student.howest.be>
2021-01-25 14:04:28 +0100

    Create interface classes

---
+++ ClassLibrary/Interfaces/ICategory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Interfaces
+{
+    public interface ICategory
+    {
+        string CategoryName { get; set; }
+    }
+}
---
+++ ClassLibrary/Interfaces/ITask.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary.Classes.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary.Interfaces
+{
+    public interface ITask
+    {
+        string TaskName { get; set; }
+        ICategory CategoryName { get; set; }
+
+    }
+}
