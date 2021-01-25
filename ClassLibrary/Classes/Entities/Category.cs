255ba18851414ba7b039bc65791bc160359fecd0
jbmaene1998 <jean-baptiste.maene@student.howest.be>
2021-01-25 14:36:51 +0100

    Add properties to classes

--- ClassLibrary/Classes/Entities/Category.cs
+++ ClassLibrary/Classes/Entities/Category.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using ClassLibrary.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ClassLibrary.Classes.Entities
 {
-    class Category
+    public abstract class Category : ICategory
     {
+        private string categoryName;
+
+        public string CategoryName
+        {
+            get { return categoryName; }
+            set { categoryName = value; }
+        }
+
     }
 }
--- ClassLibrary/Classes/Entities/Task.cs
+++ ClassLibrary/Classes/Entities/Task.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using ClassLibrary.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ClassLibrary
 {
-    class Task
+    public abstract class Task : ITask
     {
+        private string taskName;
+        private ICategory categoryName;
+
+        public string TaskName
+        {
+            get { return taskName; }
+            set { taskName = value; }
+        }
+        public ICategory CategoryName
+        {
+            get { return categoryName; }
+            set { categoryName = value; }
+        }
     }
 }
