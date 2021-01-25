0c3451887e097aef47f4608211c0b13860e89d1a
jbmaene1998 <jean-baptiste.maene@student.howest.be>
2021-01-25 14:56:55 +0100

    Modify MainWindow

--- ClassLibrary/Classes/Services/TaskServices.cs
+++ ClassLibrary/Classes/Services/TaskServices.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using ClassLibrary.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ClassLibrary.Services
 {
-    class TaskServices
+    public class TaskServices
     {
+        public List<Task> tasks;
+
+        public TaskServices()
+        {
+            tasks = new List<Task>();
+        }
+        public void CreateTask()
+        { 
+        }
     }
 }
--- ToDoList_No_DBS/MainWindow.xaml.cs
+++ ToDoList_No_DBS/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassLibrary.Classes.Entities;
+using ClassLibrary.Services;
 
 namespace ToDoList_No_DBS
 {
@@ -23,6 +25,7 @@ namespace ToDoList_No_DBS
         public MainWindow()
         {
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            TaskServices TS = new TaskServices();
             InitializeComponent();
         }
     }
