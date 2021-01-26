145038d9bbaaf81dba0c788164ba827e515c98ae
jbmaene1998 <jean-baptiste.maene@student.howest.be>
2021-01-26 15:48:57 +0100

    Add window winCat

---
+++ ToDoList_No_DBS/winCat.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoList_No_DBS
+{
+    /// <summary>
+    /// Interaction logic for winGroups.xaml
+    /// </summary>
+    public partial class winCat : Window
+    {
+        public winCat()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
