df8ae09b814af77d7d6b52783b2ca572d78472d1
jbmaene1998 <jean-baptiste.maene@student.howest.be>
2021-01-26 13:40:58 +0100

    Delete class and interface

--- ClassLibrary/Classes/Services/TaskServices.cs
+++ ClassLibrary/Classes/Services/TaskServices.cs
@@ -1,4 +1,5 @@
-﻿using ClassLibrary.Interfaces;
+﻿using ClassLibrary.Classes.Entities;
+using ClassLibrary.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,13 +9,21 @@ namespace ClassLibrary.Services
     public class TaskServices
     {
         public List<Task> tasks;
+        public List<Category> categories;
+        public Category category;
 
         public TaskServices()
         {
             tasks = new List<Task>();
+            categories = new List<Category>();
         }
-        public void CreateTask()
-        { 
+        public void CreateCategorie()
+        {
+            categories.Add(Test1)
+        }
+         
+        {
+            
         }
     }
 }
